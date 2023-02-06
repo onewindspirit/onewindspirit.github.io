f3d27e31572341b6b6235fc095dccaf95a537430
onewindspirit <maxanderson26@gmail.com>
2023-02-06 14:55:27 -0500

    added import line

--- js/gltf_loader.js
+++ js/gltf_loader.js
@@ -1,9 +1,9 @@
-import * as THREE from '/three';
+import * as THREE from '../three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+/*import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';*/
 
 let camera, scene, renderer;
 
